0239776b3a9c2f52f1f33c00fa3c058740cdaff1
Finol12 <fatemeh.loni12@gmail.com>
2023-07-20 01:02:18 +0200

    3 training models

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import r2_score
+import xgboost as xgb
+
 from src import utils
+from src import ML
 
 utils.cleaning_data("./data/raw-data.csv")
 
@@ -23,3 +30,30 @@ utils.house_apartment_hist(df)
 utils.house_and_partment_price_per_sqm(df)
 
 
+apartment_df, house_df=ML.get_df(df)
+
+
+##Linear Regression model
+train_score_apartment, test_score_apartment=ML.train_linear_regression(apartment_df, "Price", test_size=0.2, random_state=0)
+train_score_house, test_score_house=ML.train_linear_regression(house_df, "Price", test_size=0.2, random_state=0)
+print("train_score_apartment:  ",train_score_apartment)
+print("test_score_apartment:  ",test_score_apartment)
+
+print("train_score_house:  ",train_score_house)
+print("test_score_house:  ",test_score_house)
+
+
+####DecisionTRee model
+r2_apartment=ML.train_DecisionTree_regression(apartment_df, "Price", test_size=0.2, random_state=0)
+r2_house=ML.train_DecisionTree_regression(house_df, "Price", test_size=0.2, random_state=0)
+
+print("DecisionTRee R-squared (Apartment):", r2_apartment)
+print("DecisionTRee R-squared (House):", r2_house)
+
+
+####xgboost model
+r2_apartment=ML.train_xgboost(apartment_df, "Price", test_size=0.2, random_state=0)
+r2_house=ML.train_xgboost(house_df, "Price", test_size=0.2, random_state=0)
+
+print("xgboost R-squared (Apartment):", r2_apartment)
+print("xgboost R-squared (House):", r2_house)
---
+++ src/ML.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import numpy as np 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import r2_score
+import xgboost as xgb
+
+# Process and split the input DataFrame into separate DataFrames for apartments and houses
+def get_df(df):
+
+    # Convert boolean columns to integers (0 for False and 1 for True)
+    df['Swimming_pool'] = df['Swimming_pool'].astype(int)
+    df['Garden'] = df['Garden'].astype(int)
+    df['Terrace'] = df['Terrace'].astype(int)
+    df['Open Fire'] = df['Open Fire'].astype(int)
+    df['Furnished'] = df['Furnished'].astype(int)
+    # Perform one-hot encoding for the 'province' column
+    df = pd.get_dummies(df, columns=['province','District'])
+    # Filter and process apartment data
+    apartment_df = df[df["Type"] == "apartment"]
+    columns_to_drop_apartment = ['URL','Listing_ID','Price_per_sqm','Type', 'Subtype','Listing_address','Postal_code', 'Locality', 'Kitchen','State of the building']
+    apartment_df = apartment_df.drop(columns= columns_to_drop_apartment)
+    # apartment_df.to_csv('apartment_df.csv', index=False)
+    # Filter and process house data
+    house_df = df[df["Type"] == "house"]
+    columns_to_drop_house =  ['URL', 'Listing_ID','Type', 'Price_per_sqm','Subtype','Listing_address','Postal_code', 'Locality', 'Kitchen','State of the building']
+    house_df = house_df.drop(columns= columns_to_drop_house)
+    # house_df.to_csv('house_df.csv', index=False)
+    # Return the separate DataFrames for apartments and houses
+    return apartment_df, house_df
+
+
+# Train a linear regression model on the input DataFrame.
+def train_linear_regression(df, target_col, test_size=0.2, random_state=0):
+    # Split the DataFrame into feature matrix (X) and target vector (y)
+    X = df.drop(columns=[target_col]).to_numpy()
+    y = df[target_col].to_numpy().reshape(-1, 1)
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+    # Create a linear regression model
+    regressor = LinearRegression()
+    # Train the linear regression model using the training data
+    regressor.fit(X_train, y_train)
+    # Calculate the coefficient of determination (R^2) on the training set
+    train_score = regressor.score(X_train, y_train)
+    # Calculate the coefficient of determination (R^2) on the test set
+    test_score = regressor.score(X_test, y_test)
+
+    return train_score, test_score
+
+
+def train_DecisionTree_regression(df, target_col, test_size=0.2, random_state=0):
+     # Split the DataFrame into feature matrix (X) and target vector (y)
+    X = df.drop(columns=[target_col]).to_numpy()
+    y = df[target_col].to_numpy().reshape(-1, 1)
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+    # Train the DecisionTree model 
+    dt_regressor = DecisionTreeRegressor()
+    # Fit the models to the training data
+    dt_regressor.fit(X_train,y_train)
+    # Predict on the test data
+    y_pred = dt_regressor.predict(X_test)
+    # Calculate R-squared for apartment and house models
+    r2 = r2_score(y_test, y_pred)
+    return r2
+    
+
+
+def train_xgboost(df, target_col, test_size=0.2, random_state=0):
+    # Split the DataFrame into feature matrix (X) and target vector (y)
+    X = df.drop(columns=[target_col]).to_numpy()
+    y = df[target_col].to_numpy().reshape(-1, 1)
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+    # Train the xgboost model 
+    xgb_regressor = xgb.XGBRegressor()
+    # Fit the models to the training data
+    xgb_regressor.fit(X_train,y_train)
+    # Predict on the test data
+    y_pred = xgb_regressor.predict(X_test)
+    # Calculate R-squared for apartment and house models
+    r2 = r2_score(y_test, y_pred)
+    return r2
+   
